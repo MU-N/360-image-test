2c40744ae08893491d5c47e404f32609accac42d
MO Nasser <39590845+MU-N@users.noreply.github.com>
2022-01-03 22:00:04 +0200

    first commit

---
+++ Assets/__Project Files/Scripts/CameraRotaion.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotaion : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    bool isDragging = false;
+
+    float startMouseX;
+    float startMouseY;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) && !isDragging)
+        {
+            
+            isDragging = true;
+
+            startMouseX = Input.mousePosition.x;
+            startMouseY = Input.mousePosition.y;
+        }
+        else if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (isDragging)
+        {
+            float endMouseX = Input.mousePosition.x;
+            float endMouseY = Input.mousePosition.y;
+
+            float diffX = endMouseX - startMouseX;
+            float diffY = endMouseY - startMouseY;
+
+            float newCenterX = Screen.width / 2 + diffX;
+            float newCenterY = Screen.height / 2 + diffY;
+
+            Vector3 LookHerePoint = cam.ScreenToWorldPoint(new Vector3(newCenterX, newCenterY , cam.nearClipPlane));
+
+            transform.LookAt(LookHerePoint);
+
+            startMouseX = endMouseX;
+            startMouseY = endMouseY;
+        }
+    }
+
+#endif
+}
